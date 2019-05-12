1448d34b39c6dd5cc943a644039560fbdc82c61c
Mikail Bagishov <bagishov.mikail@yandex.ru>
2019-05-12 21:13:32 +0300

    Add option to skip existing files

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ struct Options {
     prefix: PathBuf,
     #[structopt(long = "file", short = "f")]
     file: Vec<PathBuf>,
+    #[structopt(long = "skip-exist", short = "s")]
+    skip_existing: bool,
 }
 
 fn ensure_dir(file: &Path) {
@@ -22,22 +24,23 @@ fn ensure_dir(file: &Path) {
     }
 }
 
-fn copy_recurse(src: &Path, dest: &Path) {
+fn copy_recurse(src: &Path, dest: &Path, skip_exist: bool) {
     use fs_extra::{dir, copy_items};
-    let copy_options = dir::CopyOptions::new();
+    let mut copy_options = dir::CopyOptions::new();
+    copy_options.skip_exist = skip_exist;
 
     let src = vec![src];
     let dest = dest.parent().unwrap();
     copy_items(&src, dest, &copy_options).expect(&format!("couldn't copy {:?} to {:?} recursively", &src[0], dest));
 }
 
-fn process(file: &Path, prefix: &Path) {
+fn process(file: &Path, prefix: &Path, skip_exist: bool) {
     let is_symlink = fs::metadata(file).expect(&format!("couldn't get metadata on {:?}", file)).file_type().is_symlink();
     if !is_symlink {
         let mut dest = prefix.to_owned();
         dest.push(file.strip_prefix(PathBuf::from("/")).unwrap());
         ensure_dir(&dest);
-        copy_recurse(file, &dest);
+        copy_recurse(file, &dest, skip_exist);
         return;
     }
     let target = file.read_link().expect(&format!("couldn't read symlink {:?} target", &file));
@@ -45,8 +48,17 @@ fn process(file: &Path, prefix: &Path) {
     target_full.push(target);
     let symlink_path = prefix.join(file);
     let symlink_target = prefix.join(&target_full);
-    std::os::unix::fs::symlink(&symlink_path, &symlink_target).expect(&format!("couldn't create symlink {:?} to {:?}", &symlink_path, &symlink_target));
-    process(&target_full, prefix);
+    match std::os::unix::fs::symlink(&symlink_path, &symlink_target) {
+        Ok(_) => {}
+        Err(err) => {
+            match err.kind() {
+                std::io::ErrorKind::AlreadyExists if skip_exist => {}
+                _ => panic!("couldn't create symlink {:?} to {:?}", &symlink_path, &symlink_target)
+            }
+        }
+   
+    }
+    process(&target_full, prefix, skip_exist);
 }
 
 fn main() {
@@ -55,6 +67,6 @@ fn main() {
         eprintln!("warning: no files to copy specified")
     }
     for f in &opt.file {
-        process(f, &opt.prefix);
+        process(f, &opt.prefix, opt.skip_existing);
     }
 }
